cc0305b6d6b065938695facf94b4ad230d948447
Wellington Gasparin <gasparin@gmail.com>
2017-11-15 22:02:03 -0200

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/gasparin/cursomc/services/CategoriaService.java
+++ src/main/java/com/gasparin/cursomc/services/CategoriaService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.gasparin.cursomc.domain.Categoria;
 import com.gasparin.cursomc.repositories.CategoriaRepository;
+import com.gasparin.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -15,6 +16,9 @@ public class CategoriaService {
 	public Categoria buscar(Integer id){
 		Categoria obj = repo.findOne(id);
 		
+		if(obj == null){
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: "+ id + ", Tipo: " + Categoria.class.getName());
+		}		
 		return obj;
 	}
 }
