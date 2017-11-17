2c988d417de2ea44ccb0e6fffd5b383c083ed867
Wellington Gasparin <gasparin@gmail.com>
2017-11-17 19:10:21 -0200

    Endpoint Cliente e correcoes

--- src/main/java/com/gasparin/cursomc/domain/Cliente.java
+++ src/main/java/com/gasparin/cursomc/domain/Cliente.java
@@ -14,6 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.gasparin.cursomc.domain.enums.TipoCliente;
 
 @Entity
@@ -28,13 +29,18 @@ public class Cliente  implements Serializable {
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
+	@JsonManagedReference
 	@OneToMany(mappedBy="cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 	
 	@ElementCollection
 	@CollectionTable(name="TELEFONE")
-	private Set<String> telefones = new HashSet();
+	private Set<String> telefones = new HashSet<>();
 
+	public Cliente(){
+		
+	}
+	
 	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
 		super();
 		this.id = id;
---
+++ src/main/java/com/gasparin/cursomc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.gasparin.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gasparin.cursomc.domain.Cliente;
+import com.gasparin.cursomc.repositories.ClienteRepository;
+import com.gasparin.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id){
+		Cliente obj = repo.findOne(id);
+		
+		if(obj == null){
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: "+ id + ", Tipo: " + Cliente.class.getName());
+		}		
+		return obj;
+	}
+}
