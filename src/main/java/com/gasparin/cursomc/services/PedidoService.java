9755b6db11845e5056af3b29a21717abf64b8243
Wellington Gasparin <gasparin@gmail.com>
2017-11-17 21:32:14 -0200

    Endpoint /pedidos/{id} feito

---
+++ src/main/java/com/gasparin/cursomc/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.gasparin.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gasparin.cursomc.domain.Pedido;
+import com.gasparin.cursomc.repositories.PedidoRepository;
+import com.gasparin.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id){
+		Pedido obj = repo.findOne(id);
+		
+		if(obj == null){
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: "+ id + ", Tipo: " + Pedido.class.getName());
+		}		
+		return obj;
+	}
+}
