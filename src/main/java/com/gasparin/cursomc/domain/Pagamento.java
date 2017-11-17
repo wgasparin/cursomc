906be430a7c3e20b022181f5836eb5f622013d21
Wellington Gasparin <gasparin@gmail.com>
2017-11-17 21:39:31 -0200

    atualizacao, utilizando somente @JsonIgore

--- src/main/java/com/gasparin/cursomc/domain/Pagamento.java
+++ src/main/java/com/gasparin/cursomc/domain/Pagamento.java
@@ -10,8 +10,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.gasparin.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
@@ -23,7 +22,7 @@ public abstract class Pagamento implements Serializable {
 	private Integer id;
 	private Integer estado;
 	
-	@JsonBackReference
+	@JsonIgnore
 	@OneToOne
 	@JoinColumn(name="pedido_id")
 	@MapsId
