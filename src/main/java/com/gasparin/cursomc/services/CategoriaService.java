3f8fc4d653877aaf0734b7a0a89af0fdefb684e4
Wellington Gasparin <gasparin@gmail.com>
2017-11-20 22:48:44 -0200

    Paginação com parametros opcionais na requisição

--- src/main/java/com/gasparin/cursomc/services/CategoriaService.java
+++ src/main/java/com/gasparin/cursomc/services/CategoriaService.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.gasparin.cursomc.domain.Categoria;
@@ -45,7 +48,14 @@ public class CategoriaService {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
 		}
 	}
+	
 	public List<Categoria> findAll(){
 		return repo.findAll();
 	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
 }
