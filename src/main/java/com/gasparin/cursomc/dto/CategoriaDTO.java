52ddb97a1909c74f0be154c048e4c60fb32d8952
Wellington Gasparin <gasparin@gmail.com>
2017-12-13 19:22:42 -0200

    Validacao sintatica com bean validation

--- src/main/java/com/gasparin/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/gasparin/cursomc/dto/CategoriaDTO.java
@@ -2,12 +2,18 @@ package com.gasparin.cursomc.dto;
 
 import java.io.Serializable;
 
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+
 import com.gasparin.cursomc.domain.Categoria;
 
 public class CategoriaDTO  implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	public Integer id;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=80, message="O tamanho deve ser entre 5 e 80 caracteres")
 	public String nome;
 
 	public CategoriaDTO() {
--- src/main/java/com/gasparin/cursomc/services/CategoriaService.java
+++ src/main/java/com/gasparin/cursomc/services/CategoriaService.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.gasparin.cursomc.domain.Categoria;
+import com.gasparin.cursomc.dto.CategoriaDTO;
 import com.gasparin.cursomc.repositories.CategoriaRepository;
 import com.gasparin.cursomc.services.exceptions.DataIntegrityException;
 import com.gasparin.cursomc.services.exceptions.ObjectNotFoundException;
@@ -56,6 +57,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto){
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 	
 }
